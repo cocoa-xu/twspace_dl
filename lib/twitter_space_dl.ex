9f056df91cf5432b87c644588c673cf08aee043b
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-26 01:34:44 +0000

    mix format

--- lib/twitter_space_dl.ex
+++ lib/twitter_space_dl.ex
@@ -31,6 +31,7 @@ defmodule TwitterSpaceDL do
   **Return**: `pid`
   """
   def new(source, id, template \\ @filename_template)
+
   def new(:space_id, id, template) do
     {:ok, pid} = GenServer.start(__MODULE__, %{from_space_id: id, template: template})
     pid
@@ -48,6 +49,7 @@ defmodule TwitterSpaceDL do
     if nil == System.find_executable("ffmpeg") do
       raise "cannot find ffmpeg"
     end
+
     GenServer.call(self_pid, :download, :infinity)
   end
 
@@ -66,18 +68,31 @@ defmodule TwitterSpaceDL do
   end
 
   @impl true
-  def handle_call(:download, _from, state = %{space_id: space_id, ets_table: ets_table, template: template}) do
+  def handle_call(
+        :download,
+        _from,
+        state = %{space_id: space_id, ets_table: ets_table, template: template}
+      ) do
     playlist = playlist_content(space_id, ets_table)
     filename = filename(space_id, ets_table, template)
     dyn_playlist = dyn_url(space_id, ets_table)
-    {:ok, %{data: %{audioSpace: %{metadata: %{state: space_state, title: title}}}}} = metadata(space_id, ets_table)
 
-    {:reply, _download(System.find_executable("ffmpeg"), filename, playlist, dyn_playlist, title, space_state), state}
+    {:ok, %{data: %{audioSpace: %{metadata: %{state: space_state, title: title}}}}} =
+      metadata(space_id, ets_table)
+
+    {:reply,
+     _download(
+       System.find_executable("ffmpeg"),
+       filename,
+       playlist,
+       dyn_playlist,
+       title,
+       space_state
+     ), state}
   end
 
   defp from_space_url(url) when is_binary(url) do
-    with [_, space_id | _] <- Regex.run(~r/spaces\/(\w+)/, url)
-    do
+    with [_, space_id | _] <- Regex.run(~r/spaces\/(\w+)/, url) do
       space_id
     else
       _ ->
@@ -114,58 +129,71 @@ defmodule TwitterSpaceDL do
       |> List.insert_at(1, "-protocol_whitelist")
       |> List.insert_at(2, "file,https,tls,tcp")
 
-    pipeline = if space_state == "Running" do
-      concat_txt = "#{title}-concat.txt"
-      download_live = ffmpeg_arg(dyn_playlist, m4a_live_filename, title)
-      merge_file =
-        ffmpeg_arg(concat_txt, m4a_filename, title)
-        |> List.insert_at(1, "-f")
-        |> List.insert_at(2, "concat")
-        |> List.insert_at(3, "-safe")
-        |> List.insert_at(4, "0")
-      [download_live, download_recorded, merge_file]
-    else
-      [download_recorded]
-    end
+    pipeline =
+      if space_state == "Running" do
+        concat_txt = "#{title}-concat.txt"
+        download_live = ffmpeg_arg(dyn_playlist, m4a_live_filename, title)
+
+        merge_file =
+          ffmpeg_arg(concat_txt, m4a_filename, title)
+          |> List.insert_at(1, "-f")
+          |> List.insert_at(2, "concat")
+          |> List.insert_at(3, "-safe")
+          |> List.insert_at(4, "0")
+
+        [download_live, download_recorded, merge_file]
+      else
+        [download_recorded]
+      end
 
     _download(ffmpeg, pipeline)
   end
 
   defp _download(_ffmpeg, []), do: :ok
+
   defp _download(ffmpeg, [args | rest]) do
-    port = Port.open({:spawn_executable, ffmpeg},
-      [:binary, :exit_status, args: args])
+    port =
+      Port.open(
+        {:spawn_executable, ffmpeg},
+        [:binary, :exit_status, args: args]
+      )
 
     receive do
       {^port, {:exit_status, 0}} -> nil
       {^port, {:exit_status, status}} -> Logger.warn("ffmpeg exit with status: #{status}")
       {^port, {:data, stdout}} -> IO.puts(Regex.replace(~r/\n/, stdout, "\r\n"))
     end
+
     _download(ffmpeg, rest)
   end
 
   defp filename(space_id, ets_table, template) do
-    with [[@filename, filename] | _] <- :ets.lookup(ets_table, @filename)
-    do
+    with [[@filename, filename] | _] <- :ets.lookup(ets_table, @filename) do
       filename
     else
       [] ->
         {:ok, %{data: %{audioSpace: %{metadata: meta}}}} = metadata(space_id, ets_table)
+
         filename =
           ~r/\%\{(\w*)\}/
           |> Regex.scan(template)
           |> format_template(template, meta)
+
         true = :ets.insert(ets_table, {@filename, filename})
         filename
     end
   end
 
   defp format_template([], template, _meta), do: template
+
   defp format_template([[raw, key] | rest], template, meta) do
-    format_template(rest,
+    format_template(
+      rest,
       raw
       |> Regex.compile!()
-      |> Regex.replace(template, Map.get(meta, String.to_atom(key), "")), meta)
+      |> Regex.replace(template, Map.get(meta, String.to_atom(key), "")),
+      meta
+    )
   end
 
   defp write_playlist(formatted_filename, playlist) do
@@ -178,11 +206,11 @@ defmodule TwitterSpaceDL do
 
   defp playlist_content(space_id, ets_table) do
     {:ok, playlist_url_str} = playlist_url(space_id, ets_table)
+
     with {:ok, master_url} = master_url(space_id, ets_table),
          url_base = Regex.replace(~r/master_playlist.m3u8.*/, master_url, ""),
          %HTTPotion.Response{body: body, status_code: 200} <-
-           HTTPotion.get(playlist_url_str, follow_redirects: true)
-    do
+           HTTPotion.get(playlist_url_str, follow_redirects: true) do
       Regex.replace(~r/chunk_/, body, "#{url_base}chunk_")
     else
       _ ->
@@ -198,8 +226,7 @@ defmodule TwitterSpaceDL do
            HTTPotion.get(master_playlist, follow_redirects: true),
          [_, _, _, suffix | _] <- String.split(body, "\n"),
          %URI{host: host} <- URI.parse(master_playlist),
-         playlist <- "https://#{host}#{suffix}"
-    do
+         playlist <- "https://#{host}#{suffix}" do
       {:ok, playlist}
     else
       _ ->
@@ -210,15 +237,14 @@ defmodule TwitterSpaceDL do
   end
 
   defp master_url(space_id, ets_table) do
-    with [{@master_playlist, master_playlist} | _] <- :ets.lookup(ets_table, @master_playlist)
-    do
+    with [{@master_playlist, master_playlist} | _] <- :ets.lookup(ets_table, @master_playlist) do
       {:ok, master_playlist}
     else
       [] ->
         with {:ok, dyn_url} <- dyn_url(space_id, ets_table),
-             master_playlist <- Regex.replace(~r/\/audio-space\/.*/, dyn_url, "/audio-space/master_playlist.m3u8"),
-             true <- :ets.insert(ets_table, {@master_playlist, master_playlist})
-        do
+             master_playlist <-
+               Regex.replace(~r/\/audio-space\/.*/, dyn_url, "/audio-space/master_playlist.m3u8"),
+             true <- :ets.insert(ets_table, {@master_playlist, master_playlist}) do
           {:ok, master_playlist}
         else
           _ ->
@@ -230,51 +256,70 @@ defmodule TwitterSpaceDL do
   end
 
   defp dyn_url(space_id, ets_table) do
-    with [{@dyn_url, dyn_url} | _] <- :ets.lookup(ets_table, @dyn_url)
-    do
+    with [{@dyn_url, dyn_url} | _] <- :ets.lookup(ets_table, @dyn_url) do
       {:ok, dyn_url}
     else
-       [] ->
-         {:ok, meta} = metadata(space_id, ets_table)
-         case meta do
-           %{data: %{audioSpace: %{metadata: %{state: "Ended", is_space_available_for_replay: false}}}} ->
-             Logger.error("Space has ended but it is not available for replay")
-             :error
-           %{data: %{audioSpace: %{metadata: %{state: "Ended", is_space_available_for_replay: true, media_key: media_key}}}} ->
-             status_url = @live_video_stream_status_endpoint <> media_key
-             with %HTTPotion.Response{body: body, status_code: 200} <-
-                    HTTPotion.get(status_url, follow_redirects: true, headers: [
-                      authorization: get_authorization(),
-                      cookie: "auth_token="
-                    ]),
-                  status <- Jason.decode!(body, keys: :atoms),
-                  %{source: %{location: dyn_url}} <- status,
-                  true <- :ets.insert(ets_table, {@dyn_url, dyn_url})
-               do
-               {:ok, dyn_url}
-             else
-               _ ->
-                 Logger.error("Space is not available")
-                 :error
-             end
-         end
+      [] ->
+        {:ok, meta} = metadata(space_id, ets_table)
+
+        case meta do
+          %{
+            data: %{
+              audioSpace: %{metadata: %{state: "Ended", is_space_available_for_replay: false}}
+            }
+          } ->
+            Logger.error("Space has ended but it is not available for replay")
+            :error
+
+          %{
+            data: %{
+              audioSpace: %{
+                metadata: %{
+                  state: "Ended",
+                  is_space_available_for_replay: true,
+                  media_key: media_key
+                }
+              }
+            }
+          } ->
+            status_url = @live_video_stream_status_endpoint <> media_key
+
+            with %HTTPotion.Response{body: body, status_code: 200} <-
+                   HTTPotion.get(status_url,
+                     follow_redirects: true,
+                     headers: [
+                       authorization: get_authorization(),
+                       cookie: "auth_token="
+                     ]
+                   ),
+                 status <- Jason.decode!(body, keys: :atoms),
+                 %{source: %{location: dyn_url}} <- status,
+                 true <- :ets.insert(ets_table, {@dyn_url, dyn_url}) do
+              {:ok, dyn_url}
+            else
+              _ ->
+                Logger.error("Space is not available")
+                :error
+            end
+        end
     end
   end
 
   defp metadata(space_id, ets_table) when is_binary(space_id) do
-    with [{@metadata, meta} | _] <- :ets.lookup(ets_table, @metadata)
-    do
+    with [{@metadata, meta} | _] <- :ets.lookup(ets_table, @metadata) do
       {:ok, meta}
     else
       [] ->
-        params = "?variables=%7B%22id%22%3A%22#{space_id}%22%2C%22isMetatagsQuery%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withBirdwatchPivots%22%3Afalse%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withReplays%22%3Atrue%2C%22withScheduledSpaces%22%3Atrue%7D"
+        params =
+          "?variables=%7B%22id%22%3A%22#{space_id}%22%2C%22isMetatagsQuery%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withBirdwatchPivots%22%3Afalse%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withReplays%22%3Atrue%2C%22withScheduledSpaces%22%3Atrue%7D"
+
         get_url = @audio_space_metadata_endpoint <> params
+
         with %HTTPotion.Response{body: body, status_code: 200} <-
                HTTPotion.get(get_url, follow_redirects: true, headers: get_guest_header(ets_table)),
              meta <- Jason.decode!(body, keys: :atoms),
              %{data: %{audioSpace: %{metadata: %{media_key: _media_key}}}} <- meta,
-             true <- :ets.insert(ets_table, {@metadata, meta})
-        do
+             true <- :ets.insert(ets_table, {@metadata, meta}) do
           {:ok, meta}
         else
           _ ->
@@ -289,8 +334,7 @@ defmodule TwitterSpaceDL do
   end
 
   defp get_guest_header(ets_table) do
-    with {:ok, guest_token} <- guest_token(ets_table)
-    do
+    with {:ok, guest_token} <- guest_token(ets_table) do
       [
         authorization: get_authorization(),
         "x-guest-token": "#{guest_token}"
@@ -303,18 +347,20 @@ defmodule TwitterSpaceDL do
   end
 
   defp guest_token(ets_table, retry_times \\ 5)
+
   defp guest_token(ets_table, retry_times) when retry_times >= 0 do
-    with [{@guest_token, guest_token}|_] <- :ets.lookup(ets_table, @guest_token)
-    do
+    with [{@guest_token, guest_token} | _] <- :ets.lookup(ets_table, @guest_token) do
       Logger.info("cached guest_token: #{guest_token}")
       {:ok, guest_token}
     else
       _ ->
         with %HTTPotion.Response{body: body, status_code: 200} <-
-               HTTPotion.get("https://twitter.com/", follow_redirects: true, headers: ["User-Agent": @user_agent]),
+               HTTPotion.get("https://twitter.com/",
+                 follow_redirects: true,
+                 headers: ["User-Agent": @user_agent]
+               ),
              [_, guest_token_str | _] <- Regex.run(~r/gt=(\d{19})/, body),
-             true <- :ets.insert(ets_table, {@guest_token, guest_token_str})
-          do
+             true <- :ets.insert(ets_table, {@guest_token, guest_token_str}) do
           Logger.info("guest_token: #{guest_token_str}")
           {:ok, guest_token_str}
         else
